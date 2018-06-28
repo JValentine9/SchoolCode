bc7d22e1f02f173a6ea6b5159aed9b1d44d8ece7
heavyodin53 <39357521+heavyodin53@users.noreply.github.com>
2018-06-27 21:31:42 -0600

    Entered some psuedocode

--- AppDev/Number Game/Number Game/Number Game/Program.cs
+++ AppDev/Number Game/Number Game/Number Game/Program.cs
@@ -8,8 +8,39 @@ namespace Number_Game
 {
     class Program
     {
+        enum Difficulty {Easy, Medium, Hard};
+
         static void Main(string[] args)
         {
+            Difficulty d;
+            //Select Difficulty Method
         }
+
+        public Difficulty SetDifficulty()
+        {
+            Difficulty setDiff = Difficulty.Easy;
+            
+            return setDiff;
+        }
+
+        public void Game()
+        {
+            //generate number
+            //boolean Correct = false
+            //boolean isInt = false
+
+            //while Correct = False
+            //while isInt = False
+            //ask for input
+                //if input is int, isInt = True
+                // else say please input int.
+            //if input > answer, say to high
+            //if input < answer, say to low
+            //if input = answer, say you are correct, set Correct = True
+
+
+           
+        }
+
     }
 }
