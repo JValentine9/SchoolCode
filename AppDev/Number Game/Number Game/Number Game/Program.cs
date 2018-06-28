8339a882373879d10d54fd8c9678ac5ed9814e13
Jacob Valentine <JValentine@student.neumont.edu>
2018-06-28 09:31:24 -0600

    completed number game

--- AppDev/Number Game/Number Game/Number Game/Program.cs
+++ AppDev/Number Game/Number Game/Number Game/Program.cs
@@ -8,39 +8,133 @@ namespace Number_Game
 {
     class Program
     {
-        enum Difficulty {Easy, Medium, Hard};
+        public enum Difficulty { Easy, Medium, Hard };
+        static Difficulty setDiff;
+        static public int inMax;
+        static public int inMin = 1;
+        static public int input;
 
         static void Main(string[] args)
         {
             Difficulty d;
-            //Select Difficulty Method
+            d = SetDifficulty();
+
+            Game(d);
         }
 
-        public Difficulty SetDifficulty()
+        static public Difficulty SetDifficulty()
         {
-            Difficulty setDiff = Difficulty.Easy;
-            
+            bool valid = false;
+
+            while (!valid)
+            {
+                Console.Write("Please set a difficulty by entering the associated number. 1. Easy 2. Medium 3. Hard");
+                int caseSwitch = int.Parse(Console.ReadLine());
+                switch (caseSwitch)
+                {
+
+                    case 1:
+                        setDiff = Difficulty.Easy;
+                        valid = true;
+                        break;
+                    case 2:
+                        setDiff = Difficulty.Medium;
+                        valid = true;
+                        break;
+                    case 3:
+                        setDiff = Difficulty.Hard;
+                        valid = true;
+                        break;
+                    default:
+                        Console.WriteLine("Please enter a valid input");
+                        break;
+                }
+            }
             return setDiff;
         }
 
-        public void Game()
+        static public void Game(Difficulty d)
         {
-            //generate number
-            //boolean Correct = false
-            //boolean isInt = false
+            Random rnd = new Random();
+            int ans;
+            int input;
+            bool correct = false;
+            int attempts = 5;
+            if (d == Difficulty.Easy)
+            {
+                ans = rnd.Next(1, 11);
+                inMax = 10;
+            }
+            else if (d == Difficulty.Medium)
+            {
+                ans = rnd.Next(1, 51);
+                inMax = 50;
+            }
+            else
+            {
+                ans = rnd.Next(1, 101);
+                inMax = 100;
+            }
 
-            //while Correct = False
-            //while isInt = False
-            //ask for input
-                //if input is int, isInt = True
-                // else say please input int.
-            //if input > answer, say to high
-            //if input < answer, say to low
-            //if input = answer, say you are correct, set Correct = True
+            while (!correct && attempts > 0)
+            {
+                input = InputValidation();
 
-
-           
+                if (input < ans)
+                {
+                    Console.WriteLine("A little low");
+                    attempts--;
+                }
+                else if (input > ans)
+                {
+                    Console.WriteLine("A little high");
+                    attempts--;
+                }
+                else
+                {
+                    Console.WriteLine("correct!");
+                    correct = true;
+                }
+            }
+            if (correct)
+            {
+                Console.WriteLine("You Win!");
+            }
+            else if (attempts < 1)
+            {
+                Console.WriteLine("Better luck next time.");
+            }
         }
 
+        static public int InputValidation()
+        {
+            bool isInt = false;
+            bool isValid = false;
+            string inputTest;
+
+            while (!isInt)
+            {
+                Console.Write($"Please enter a whole number between {inMin} and {inMax}: ");
+                inputTest = Console.ReadLine();
+
+                if (int.TryParse(inputTest, out input))
+                {
+                    if (input > inMax || input < inMin)
+                    {
+                        Console.WriteLine($"Your input must be between {inMin} and {inMax}.");
+                    }
+                    else
+                    {
+                        isValid = true;
+                    }
+                    isInt = true;
+                }
+                else
+                {
+                    Console.WriteLine($"You must input a valid integer between {inMin} and {inMax}.");
+                }
+            }
+            return input;
+        }
     }
 }
