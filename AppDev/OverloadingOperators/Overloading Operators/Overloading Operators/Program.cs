b8ed074d706724de2be5fecd8c918c25f8669265
heavyodin53 <39357521+heavyodin53@users.noreply.github.com>
2018-07-09 18:23:19 -0600

    thing

--- AppDev/OverloadingOperators/Overloading Operators/Overloading Operators/Program.cs
+++ AppDev/OverloadingOperators/Overloading Operators/Overloading Operators/Program.cs
@@ -5,11 +5,13 @@ using System.Text;
 using System.Threading.Tasks;
 
 namespace Overloading_Operators
+namespace OverloadingOperators
 {
     class Program
     {
         static void Main(string[] args)
         {
+            
         }
     }
 }
