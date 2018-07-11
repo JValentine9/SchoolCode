dc9a621e7547747bdbc708ca767e6acaab6add63
Jacob Valentine <JValentine@student.neumont.edu>
2018-07-11 09:49:57 -0600

    stuff

---
+++ AppDev/inclass/RaceDay/RaceDay/Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaceDay
+{
+    public static class DemoExtensions
+    {
+        public static int GetRandom(this int max, int min = 1)
+        {
+            if(min>max)
+            {
+                throw new ArgumentException($"Min must not be greater than max");
+            }
+        }
+
+    }
+
+
+    public class Class1
+    {
+        public static void Rcce2()
+        {
+            string hw = "Hello World";
+
+        }
+
+        public static void Race3()
+        {
+            //randomly select a number between 17 and 59 and print it to the console
+            Console.WriteLine(17.GetRandom(59));
+        }
+  
+    }
+}
