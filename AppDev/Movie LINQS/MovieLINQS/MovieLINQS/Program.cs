37a101427f525126fde253ee3dedee9c7935c286
Jacob Valentine <JValentine@student.neumont.edu>
2018-07-16 09:30:20 -0600

    Initialized MovieLinq

---
+++ AppDev/Movie LINQS/MovieLINQS/MovieLINQS/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MovieLINQS
+{
+    class Program
+    {
+        //LINQ Comprehension Syntax
+        //var evenNums = from int num in nums
+        //      where num % 2 == 0
+        //      select num;
+
+        //LINQ Extension Syntax
+        //var evenNums = nums.Where(num => num % 2 = 0);
+        //Where, Select, Orderby, OrderByDescending, GroupBy, Min, Max
+
+        //LINQ Desugarized Syntax
+        //var evenNums = Enumerable.Where(nums, Predicate1);
+        static void Main(string[] args)
+        {
+        }
+    }
+}
