0eeb5bc3c5b656e92e03a9368a5d5166ec8a6900
Jacob Valentine <JValentine@student.neumont.edu>
2018-07-19 17:11:35 -0600

    Completed Delegation Assignment

---
+++ AppDev/DelegatePractice/ValentineJ_DelegatePractice/ValentineJ_DelegatePractice/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Numerics;
+
+namespace ValentineJ_DelegatePractice
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="nums"></param>
+    public delegate void Mathematician(params int[] nums);
+
+    class Program
+    {
+        public static Mathematician Einstein;
+        public static string input;
+
+        static void Main(string[] args)
+        {
+            Einstein += ParamMath.Add;
+            Einstein += ParamMath.Subtract;
+            Einstein += ParamMath.Multiply;
+            Einstein += ParamMath.Divide;
+            Einstein += ParamMath.Mod;
+
+            GetNumbers();
+            Einstein(ConvertToStringArray());
+            Console.Write("Press Any Key To Return...");
+            Console.ReadLine();
+            
+        }
+
+        static void GetNumbers()
+        {
+            Console.Write("Please input a number of valid integers seperated by commas and spaces: ");
+            input = Console.ReadLine();
+        }
+
+        static int[] ConvertToStringArray()
+        {
+            input.Trim(',');
+            input.Replace(" ", "");
+            int[] nums = input.Split(',').Select(str => int.TryParse(str, out int val) ? val : 0).Cast<int>().ToArray();
+            return nums;
+        }
+    }
+
+    /// <summary>
+    /// A class for doing math based upon an unknown number of integers
+    /// </summary>
+    class ParamMath
+    {
+        /// <summary>
+        /// A method to add an unknown number of integers together. Goes from first to last, in the order they are entered. Prints the sum at the end of the task
+        /// </summary>
+        /// <param name="nums">The integers to be added together, from first to last</param>
+        public static void Add(params int[] nums)
+        {
+            long ans = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                ans += nums[i];
+            }
+
+            Console.WriteLine($"Sum: {ans}");
+        }
+
+        /// <summary>
+        /// A method to subtract an unknown number of integers from each other. Goes from first to last, in the order they are entered. Prints the difference at the end of the task.
+        /// </summary>
+        /// <param name="nums">The numbers to be subtracted from each other, from first to last</param>
+        public static void Subtract(params int[] nums)
+        {
+            int ans = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                ans -= nums[i];
+            }
+
+            Console.WriteLine($"Difference: {ans}");
+        }
+
+        /// <summary>
+        /// A method to multiply an unknown number of integers by each other. Goes from first to last, in the order they are entered. Prints the product at the end of the task.
+        /// </summary>
+        /// <param name="nums">The numbers to be multiplied by each other, from first to last</param>
+        public static void Multiply(params int[] nums)
+        {
+            long ans = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                ans *= nums[i];
+            }
+
+            Console.WriteLine($"Product: {ans}");
+        }
+
+        /// <summary>
+        /// A method to divide an unknown number of integers by each other. Goes from first to last, in the order they are entered. Prints the quotient at the end of the task.
+        /// </summary>
+        /// <param name="nums">The numbers to be divided by each other, from first to last</param>
+        public static void Divide(params int[] nums)
+        {
+            double ans = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    ans /= nums[i];
+                }
+                else
+                {
+                    throw new DivideByZeroException();
+                }
+            }
+
+            Console.WriteLine($"Quotient: {ans}");
+        }
+
+        /// <summary>
+        /// A method to modulate an unknown number of integers by each other. Goes from first to last, in the order they are entered. Prints the remainder at the end of the task.
+        /// </summary>
+        /// <param name="nums">The numbers to be modulated by each other, from first to last.</param>
+        public static void Mod(params int[] nums)
+        {
+            double ans = nums[0];
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != 0)
+                {
+                    ans %= nums[i];
+                }
+                else
+                {
+                    throw new DivideByZeroException();
+                }
+            }
+
+            Console.WriteLine($"Remainder: {ans}");
+        }
+    }
+}
---
+++ AppDev/inclass/DelegateDemo/DelegateDemo/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DelegateDemo
+{
+    public delegate void PrintDelegate(string input);
+
+    public static class NewsPublisher
+    {
+        public static PrintDelegate newsDel;
+
+
+        public static int num;
+
+        private static string currentHeadline = "Nothing to Report";
+
+        public static void UpdateHeadline(string headline)
+        {
+            currentHeadline = headline;
+            if (newsDel != null)
+            {
+                newsDel.Invoke(currentHeadline);
+            }
+        }
+
+    }
+    
+    public static class TVAnchor
+    {
+        public static void ReportTheNews(String News)
+        {
+            Console.WriteLine($"This just in: {News}");
+        }
+    }
+
+    public static class Blogger
+    {
+        public static void HashtagPost(string knowledge)
+        {
+            Console.WriteLine($"The Establishment just said \"{knowledge}\"but that's not the ENTIRE truth, is it? ");
+        }
+    }
+
+    public static class BurnOut
+    {
+        public static void HearStuff(String stuff)
+        {
+            Console.WriteLine("Dude, did you say something?");
+        }
+    }
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            //subscribe our listeners to the delegate
+            NewsPublisher.newsDel += TVAnchor.ReportTheNews;
+            NewsPublisher.newsDel += Blogger.HashtagPost;
+            NewsPublisher.newsDel += BurnOut.HearStuff;
+
+            string news = "Playstation Nation is now a sovereign sounty. XBots swear to invade.";
+
+            NewsPublisher.UpdateHeadline(news);
+        }
+    }
+}
