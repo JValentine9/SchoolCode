463ab9b55fc265316738bdd7a5af958ff76a1f29
Jacob Valentine <JValentine@student.neumont.edu>
2018-08-06 12:01:24 -0600

    Worked on stuff. Stuff has happened!

--- AppDev/Conway/ValentineJ_Conway/ValentineJ_Conway/MainPage.xaml.cs
+++ AppDev/Conway/ValentineJ_Conway/ValentineJ_Conway/MainPage.xaml.cs
@@ -3,8 +3,11 @@ using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
+using ValentineJ_Conway.Converters;
+using ValentineJ_Conway.Models;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.UI;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -12,6 +15,7 @@ using Windows.UI.Xaml.Data;
 using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
+using Windows.UI.Xaml.Shapes;
 
 // The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
 
@@ -24,6 +28,8 @@ namespace ValentineJ_Conway
     {
         private int Rows;
         private int Columns;
+        BoolToBrushConverter con = new BoolToBrushConverter();
+
         public MainPage()
         {
             this.InitializeComponent();
@@ -31,7 +37,57 @@ namespace ValentineJ_Conway
 
         private void Input_Click(object sender, RoutedEventArgs e)
         {
+            Rows = int.Parse(RowInput.Text);
+            Columns = int.Parse(ColumnInput.Text);
 
+            if (Rows < 10 || Columns < 10)
+            {
+                ErrorBox.Text = "Input too low, minimum input is 10";
+            }
+            else if (Rows > 100 || Columns > 100)
+            {
+                ErrorBox.Text = "Input too high, maximum input is 100";
+            }
+            else
+            {
+                CreateGrid();
+            }
+
+            //FirstGen();
+        }
+
+        private void FirstGen()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void CreateGrid()
+        {
+            for (int x = 0; x < Rows; x++)
+            {
+                for (int y = 0; y < Columns; y++)
+                {
+                    Cell cell = new Cell();
+                    Rectangle rect = new Rectangle();
+                    rect.AddHandler(TappedEvent, new TappedEventHandler(flipcell), true);
+                    rect.DataContext = cell;
+                    Binding b = new Binding();
+                    b.Path = new PropertyPath("IsLive");
+                    b.Mode = BindingMode.OneWay;
+                    b.Converter = con;
+                    rect.SetBinding(Shape.FillProperty, b);
+
+                    PlayArea.Children.Add(rect);
+                    Grid.SetColumn(rect, y);
+                    Grid.SetRow(rect, x);
+                }
+            }
+        }
+
+        private void flipcell(object sender, TappedRoutedEventArgs e)
+        {
+            var model = (Cell)((FrameworkElement)sender).DataContext;
+            model.Toggle();
         }
     }
 }
