498561e6814141dbe25e5f51797163019183b51d
Jacob Valentine <JValentine@student.neumont.edu>
2018-08-24 12:03:41 -0600

    stuff

---
+++ AppDev/Battleship/Battleship/Battleship/Models/Field.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship.Models
+{
+    enum SquareState {Water, HitShip, Miss, SafeShip};
+
+    class Field
+    {
+        public Ship Battleship = new Ship { };
+        public SquareState[,] PlayArea = new SquareState[10, 10];
+    }
+}
---
+++ AppDev/Battleship/Battleship/Battleship/Models/Ship.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Battleship.Models
+{
+    class Ship
+    {
+        public string Name { get; set; }
+
+        public int Length { get; set; }
+
+        public bool IsVertical { get; set; }
+
+        public bool IsSunk { get; set; }
+
+        public int Start { get; set; }
+
+        public int End { get; set; }
+    }
+}
